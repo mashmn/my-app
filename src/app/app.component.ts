680430093dc91a8cd8be0947686aab7deaa63e87
Mahesh Nair <mnair@adsworks.com>
2017-07-07 12:14:20 -0400

    add initial files

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  styles: [`
+      h3 {
+        color:dodgerblue;
+      }
+    `]
+})
+export class AppComponent {
+  name = '';
+  title = 'Hey!'
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers', // class like in css but not by id
+  templateUrl: './servers.component.html',
+  // template: `<app-server></app-server><app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No Server was created!";
+  serverName='Testserver';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000);
+  }
+
+  ngOnInit() {
+  }
+
+ onCreateServer() {
+   this.serverCreated = true;
+   this.servers.push(this.serverName);
+   this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+ }
+
+ onUpdateServerName(event: Event) {
+   this.serverName = (<HTMLInputElement>event.target).value;
+ }
+}
